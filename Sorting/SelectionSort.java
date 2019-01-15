73d761dcb1c9b007478e9904e79bffb15f6f2574
Ivan Turkin <ivanturkinrussia@gmail.com>
2019-01-15 21:17:00 +0300

    SelectionSort implementation for Java was added

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+
+    public static void selectionSort(int[] array) {
+        //choose the smallest element in every iteration
+        for (int i = 0; i < array.length - 1; i++) {
+            int smallest = array[i];
+            int smallestIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < smallest) { //compare every element in the unsorted part of array with the smallest element
+                    smallest = array[j];
+                    smallestIndex = j;
+                }
+            }
+            //swap elements
+            array[smallestIndex] = array[i];
+            array[i] = smallest;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 16, 23, 43, 55, 58, 61};
+        selectionSort(array);
+
+        for(int number: array) {
+            System.out.print(number + " ");
+        }
+    }
+}
