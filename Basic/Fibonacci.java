b57a9fbf546c5123db94121cc86262d1719105c9
Ivan Turkin <ivanturkinrussia@gmail.com>
2020-02-16 12:04:17 +0300

    Fibonnaci implementation in Java using simple cycle

--- Basic/Fibonacci.java
+++ Basic/Fibonacci.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Fibonacci {
 
     public static void main(String[] args) {
         for (int n = 0; n <= 10; n++) {
@@ -9,6 +9,15 @@ public class Main {
     private static long fib(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-        return fib(n - 1) + fib(n - 2);
+	long n1 = 0;
+	long n2 = 1;
+	long result = 0;
+	for (int i = 2; i <= n; i++) {
+		result = n1 + n2;
+		n1 = n2;
+		n2 = result;
+		
+	}
+        return result;
     }
 }
