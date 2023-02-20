233615eb2393dc8a41bf37a9ab87a4464297677f
ivanturkin <ivanturkinrussia@gmail.com>
2023-02-21 00:02:00 +0300

    LeetCode: LongestCommonPrefix problem implementation in Java

---
+++ LeetCodeTasks/easy/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        String[] strs1 = new String[] { "flower","flow","flight" };
+        System.out.println(longestCommonPrefix(strs1));
+        String[] strs2 = new String[] { "dog","racecar","car" };
+        System.out.println(longestCommonPrefix(strs2));
+    }
+
+    private static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+        }
+}
